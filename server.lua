79f0d365290c099a46fe18c290e497dbd24300f6
neuh <93486145+xneuh@users.noreply.github.com>
2023-03-27 19:47:15 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,7 +1,5 @@
 ESX = exports.es_extended:getSharedObject()
 
 ESX.RegisterUsableItem('vape', function(source)
-	local xPlayer = ESX.GetPlayerFromId(source)
-	xPlayer.removeInventoryItem('vape', 1)
 	TriggerClientEvent('esx_basicneeds:onVape', source)
 end)
